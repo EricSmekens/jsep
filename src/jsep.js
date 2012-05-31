1f8640dee8dc753500eb5905cc0676520ac3bf05
Stephen Oney <swloney@gmail.com>
2012-05-31 18:25:38 -0400

    Passing more jslint tests

--- src/jsep.js
+++ src/jsep.js
@@ -1,17 +1,13 @@
 (function (root) {
 	"use strict";
 
-	var unary_ops = ["-", "!"],
-		// Must put ops with common substrings to have most specific before less specific (<= before <)
-		binary_ops = ["+", "-", "/", "*", "&&", "and", "||", "or", "&", "|", "==", "===", ">=", "<=", "<<", ">>", "<", ">"]; 
+	var unary_ops = ["-", "!"], // Permissible unary operations
+		binary_ops = ["+", "-", "*", "/", "&&", "||", "&", "|", "<<", ">>", "===", "==", ">=", "<=",  "<", ">"]; //Permissible binary operations
 
-	var start_str_regex = new RegExp("^['\"]");
-
-	var ltrim_regex = /^\s+/;
-	var ltrim = function (str) {
-		return str.replace(ltrim_regex,'');
-	};
-
+	var ltrim_regex = /^\s+/,
+		ltrim = function (str) {
+			return str.replace(ltrim_regex, '');
+		};
 
 	var Parser = function (expr, options) {
 		this.expr = expr;
@@ -26,49 +22,48 @@
 			var rv = [];
 			while (this.buffer) {
 				this.gobble_expression();
-				if(this.curr_node === false) {
+				if (this.curr_node === false) {
 					throw new Error("Unexpected " + this.buffer);
 				}
 				rv.push(this.curr_node);
 			}
 			if (rv.length === 1) {
-				return rv[0]
+				return rv[0];
 			} else {
 				return {
-					type: "compound"
-					, statements: rv
+					type: "compound",
+					statements: rv
 				};
 			}
 		};
 
 		proto.gobble_expression = function () {
-			this.curr_node = null;
-			
 			var node;
+			this.curr_node = null;
 
-			do  {
+			do {
 				this.buffer = ltrim(this.buffer);
 				node = false;
-				if(node === false) {
-					node = this.gobble_token()
+				if (node === false) {
+					node = this.gobble_token();
 				}
-				if(node === false) {
-					node = this.parse_fn_call()
+				if (node === false) {
+					node = this.parse_fn_call();
 				}
-				if(node === false) {
-					node = this.parse_parens()
+				if (node === false) {
+					node = this.parse_parens();
 				}
-				if(node === false) {
-					node = this.parse_binary_op()
+				if (node === false) {
+					node = this.parse_binary_op();
 				}
-				if(node === false) {
-					node = this.parse_unary_op()
+				if (node === false) {
+					node = this.parse_unary_op();
 				}
 
-				if(node) {
+				if (node) {
 					this.curr_node = node;
 				}
-			} while(node);
+			} while (node);
 
 
 			return this.curr_node;
@@ -96,13 +91,13 @@
 		var var_regex = new RegExp("^([A-Za-z_$][A-Za-z_$0-9]*)");
 		proto.parse_variable = function () {
 			var match = this.buffer.match(var_regex);
-			if (match) {// We're dealing with a variable name
+			if (match) { // We're dealing with a variable name
 				var var_name = match[1];
 				this.buffer = this.buffer.substr(match[0].length);
 				return {
-					type: "var"
-					, var_name: var_name
-					, text: match[0]
+					type: "var",
+					var_name: var_name,
+					text: match[0]
 				};
 			}
 			return false;
@@ -117,14 +112,14 @@
 				var prop_node = this.parse_variable();
 				if (prop_node) {
 					var node = {
-						type: "prop"
-						, subtype: "dot"
-						, parent: this.curr_node
-						, child: prop_node
+						type: "prop",
+						subtype: "dot",
+						parent: this.curr_node,
+						child: prop_node
 					};
 					return node;
 				} else {
-					throw new Error("Unexpected property '"+this.buffer[0]+"'");
+					throw new Error("Unexpected property '" + this.buffer[0] + "'");
 				}
 			}
 			return false;
@@ -139,46 +134,51 @@
 				this.buffer = this.buffer.substr(match[0].length);
 				if (prop_node) {
 					var node = {
-						type: "prop"
-						, subtype: "square_brackets"
-						, parent: this.curr_node
-						, child: prop_node
-						, outer_text: match[0]
-						, inner_text: match[1]
+						type: "prop",
+						subtype: "square_brackets",
+						parent: this.curr_node,
+						child: prop_node,
+						outer_text: match[0],
+						inner_text: match[1]
 					};
 					return node;
 				} else {
-					throw new Error("Unexpected property '"+match[1]+"'");
+					throw new Error("Unexpected property '" + match[1] + "'");
 				}
 			}
 			return false;
 		};
+		var start_str_regex = new RegExp("^['\"]");
 		var number_regex = new RegExp("^(\\d+(\\.\\d+)?)");
-		proto.parse_constant = function() {
-			var match;
-			if (match = this.buffer.match(number_regex)) {
+		proto.parse_constant = function () {
+			var match, node;
+			match = this.buffer.match(number_regex);
+			if (match) {
 				this.buffer = this.buffer.substr(match[0].length);
-				var node = {
-					type: "constant"
-					, text: match[0]
-					, value: parseFloat(match[1])
+				node = {
+					type: "constant",
+					text: match[0],
+					value: parseFloat(match[1])
 				};
 				return node;
-			} else if (match = this.buffer.match(start_str_regex)) {
-				var quote_type = match[0];
-				var matching_quote_index = this.buffer.indexOf(quote_type, quote_type.length);
+			} else {
+				match = this.buffer.match(start_str_regex);
+				if (match) {
+					var quote_type = match[0];
+					var matching_quote_index = this.buffer.indexOf(quote_type, quote_type.length);
 
-				if (matching_quote_index >= 0) {
-					var content = this.buffer.substring(1, matching_quote_index);
-					var node = {
-						type: "constant"
-						, text: this.buffer.substring(0, matching_quote_index + 1)
-						, value: content
-					};
-					this.buffer = this.buffer.substr(matching_quote_index + 1);
-					return node;
-				} else {
-					throw new Error("Unclosed quote in " + match[0]);
+					if (matching_quote_index >= 0) {
+						var content = this.buffer.substring(1, matching_quote_index);
+						node = {
+							type: "constant",
+							text: this.buffer.substring(0, matching_quote_index + 1),
+							value: content
+						};
+						this.buffer = this.buffer.substr(matching_quote_index + 1);
+						return node;
+					} else {
+						throw new Error("Unclosed quote in " + match[0]);
+					}
 				}
 			}
 			return false;
@@ -189,7 +189,7 @@
 		proto.parse_fn_call = function () {
 			if (this.curr_node && (this.curr_node.type === "prop" || this.curr_node.type === "var" || this.curr_node.type === "fn_call")) {
 				var match;
-				if(match = this.buffer.match(open_paren_regex)) {
+				if (match = this.buffer.match(open_paren_regex)) {
 					var arg_node = false;
 					this.buffer = this.buffer.substr(1); // Kill the open paren
 					var args = [];
@@ -207,9 +207,9 @@
 					} while(arg_node);
 					this.curr_node = old_curr_node;
 					var node = {
-						type: "fn_call"
-						, args: args
-						, fn: this.curr_node
+						type: "fn_call",
+						args: args,
+						fn: this.curr_node
 					};
 					return node;
 				}
@@ -231,8 +231,8 @@
 				if(match = this.buffer.match(close_paren_regex)) {
 					this.buffer = this.buffer.substr(match[0].length);
 					var node = {
-						type: "grouping"
-						, contents: contents
+						type: "grouping",
+						contents: contents
 					};
 					return node;
 				} else {
@@ -250,10 +250,10 @@
 					var operand = this.gobble_expression();
 
 					var node = {
-						type: "op"
-						, subtype: "unary"
-						, op: unary_op
-						, operands: [operand]
+						type: "op",
+						subtype: "unary",
+						op: unary_op,
+						operands: [operand]
 					};
 					return node;
 				}
@@ -271,12 +271,11 @@
 						this.curr_node = null;
 						var operand_2 = this.gobble_expression();
 
-
 						var node = {
-							type: "op"
-							, subtype: "binary"
-							, op: binary_op
-							, operands: [operand_1, operand_2]
+							type: "op",
+							subtype: "binary",
+							op: binary_op,
+							operands: [operand_1, operand_2]
 						};
 						return node;
 					}
@@ -286,7 +285,7 @@
 		};
 	}(Parser));
 
-	root.jsep = function(expr, options) {
+	root.jsep = function (expr, options) {
 		var parser = new Parser(expr, options);
 		return parser.tokenize();
 	};
