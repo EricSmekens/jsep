bce0252c44c7fbe54c965fcdd92c69b269af53ea
morsh <mor.shemesh@gmail.com>
2016-11-01 12:54:01 +0200

    updating after checking with real project

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,74 +1,45 @@
 
 declare module 'jsep' {
-  export class jsep {
-    type: string;  
 
-    constructor(expression: jsep | string);
+  namespace jsep {
+    export interface IExpression {
+      type: string;  
+    }
+
+    export interface ILiteral extends IExpression {
+      type: "Literal";
+      value: any;
+      raw: string;
+    }
+
+    export interface IIdentifier extends IExpression {
+      type: 'Identifier'
+      name: string;
+    }
+
+    export interface IBinaryExpression extends IExpression {
+      type: 'BinaryExpression';
+      operator: string;
+      left: IExpression;
+      right: IExpression;
+    }
+
+    export interface IUnaryExpression extends IExpression {
+      type: 'UnaryExpression';
+      operator: string;
+      argument: IExpression;
+      prefix: boolean;
+    }
+
+    export interface IMemberExpression extends IExpression {
+      type: 'MemberExpression';
+      computed: boolean;
+      object: IExpression;
+      property: IExpression;
+    }
   }
 
-  export interface ILiteral extends jsep {
-    type: "Literal";
-    value: any;
-    raw: string;
-  }
+  function jsep(obj: string | jsep.IExpression): jsep.IExpression;
 
-  export interface IIdentifier extends jsep {
-    type: 'Identifier'
-    name: string;
-  }
-
-  export interface IBinaryExpression extends jsep {
-    type: 'BinaryExpression';
-    operator: string;
-    left: jsep;
-    right: jsep;
-  }
-
-  export interface ILogicalExpression extends jsep {
-    type: 'LogicalExpression';
-    operator: string;
-    left: jsep;
-    right: jsep;
-  }
-
-  export interface IConditionalExpression extends jsep {
-    type: 'ConditionalExpression';
-    test: any;
-    consequent: any;
-    alternate: any;
-  }
-
-  export interface IUnaryExpression extends jsep {
-    type: 'UnaryExpression';
-    operator: string;
-    argument: jsep;
-    prefix: boolean;
-  }
-
-  export interface IMemberExpression extends jsep {
-    type: 'MemberExpression';
-    computed: boolean;
-    object: any | jsep;
-    property: any | jsep;
-  }
-
-  export interface IThisExpression extends jsep {
-    type: "ThisExpression";
-  }
-
-  export interface ICallExpression extends jsep {
-    type: "CallExpression";
-    arguments: any[];
-    callee: any;
-  }
-
-  export interface IArrayExpression extends jsep {
-    type: "ArrayExpression";
-    elements: any[];
-  }
-
-  export interface ICompound extends jsep {
-    type: "Compound";
-    body: any[];
-  }
+  export = jsep;
 }
