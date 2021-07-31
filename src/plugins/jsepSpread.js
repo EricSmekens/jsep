bf8b0525c4eee515dd8f41fe5610664a3167431d
Shelly (6utt3rfly) <17912877+6utt3rfly@users.noreply.github.com>
2021-07-30 21:34:02 -0700

    - Remove unnecessary `env.node` check (jsep.searchHook negates the need)

--- src/plugins/jsepSpread.js
+++ src/plugins/jsepSpread.js
@@ -6,7 +6,7 @@ export default {
 		// Works in objects { ...a }, arrays [...a], function args fn(...a)
 		// NOTE: does not prevent `a ? ...b : ...c` or `...123`
 		jsep.hooks.add('gobble-token', function gobbleSpread(env) {
-			if ([0, 1, 2].every(i => this.expr.charCodeAt(this.index + i) === jsep.PERIOD_CODE) && !env.node) {
+			if ([0, 1, 2].every(i => this.expr.charCodeAt(this.index + i) === jsep.PERIOD_CODE)) {
 				this.index += 3;
 				env.node = {
 					type: 'SpreadElement',
