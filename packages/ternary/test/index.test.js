1e91c02a4b6182e1b3919ae133624763917a827f
Shelly (6utt3rfly) <17912877+6utt3rfly@users.noreply.github.com>
2021-10-13 00:46:19 -0700

    fix: ternary precedence against assignment precedence
    assignment operators are at level 0.9 (below the lowest jsep default of 1, and below the object plugin of 0.95 for :). When a ConditionalExpression is built, it now respects operators with precedence less than 0.95 to prioritize them before the conditional.
    Also added more test cases for ternary
    
    fixes #188
    fixes #189

--- packages/ternary/test/index.test.js
+++ packages/ternary/test/index.test.js
@@ -15,9 +15,200 @@ const { test } = QUnit;
 			});
 		});
 
-		test('should result in ConditionalExpression', function (assert) {
-			testParser('a ? b : c', { type: 'ConditionalExpression' }, assert);
+		test('should parse binary op with ConditionalExpression', function (assert) {
 			testParser('a||b ? c : d', { type: 'ConditionalExpression' }, assert);
+		});
+
+		test('should parse minimal ternary', (assert) => {
+			testParser('a ? b : c', {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'Identifier',
+					name: 'a'
+				},
+				consequent: {
+					type: 'Identifier',
+					name: 'b'
+				},
+				alternate: {
+					type: 'Identifier',
+					name: 'c'
+				}
+			}, assert);
+		});
+
+		test('should parse secondary ternary on consequent side', (assert) => {
+			testParser('a ? b ? c : 1 : 2', {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'Identifier',
+					name: 'a'
+				},
+				alternate: {
+					type: 'Literal',
+					value: 2,
+					raw: '2'
+				},
+				consequent: {
+					type: 'ConditionalExpression',
+					test: {
+						type: 'Identifier',
+						name: 'b'
+					},
+					consequent: {
+						type: 'Identifier',
+						name: 'c'
+					},
+					alternate: {
+						type: 'Literal',
+						value: 1,
+						raw: '1'
+					}
+				}
+			}, assert);
+		});
+
+		test('should parse secondary ternary on alternate side', (assert) => {
+			testParser('a ? b : c ? 1 : 2', {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'Identifier',
+					name: 'a'
+				},
+				consequent: {
+					type: 'Identifier',
+					name: 'b'
+				},
+				alternate: {
+					type: 'ConditionalExpression',
+					test: {
+						type: 'Identifier',
+						name: 'c'
+					},
+					consequent: {
+						type: 'Literal',
+						value: 1,
+						raw: '1'
+					},
+					alternate: {
+						type: 'Literal',
+						value: 2,
+						raw: '2'
+					}
+				}
+			}, assert);
+		});
+
+		test('should parse 3 level ternaries with mixed consequent/alternate', (assert) => {
+			testParser('a ? b ? 1 : c ? 5 : 6 : 7', {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'Identifier',
+					name: 'a'
+				},
+				alternate: {
+					type: 'Literal',
+					value: 7,
+					raw: '7'
+				},
+				consequent: {
+					test: {
+						type: 'Identifier',
+						name: 'b'
+					},
+					alternate: {
+						type: 'ConditionalExpression',
+						test: {
+							type: 'Identifier',
+							name: 'c'
+						},
+						consequent: {
+							type: 'Literal',
+							value: 5,
+							raw: '5'
+						},
+						alternate: {
+							type: 'Literal',
+							value: 6,
+							raw: '6'
+						}
+					},
+					consequent: {
+						type: 'Literal',
+						value: 1,
+						raw: '1'
+					},
+					type: 'ConditionalExpression'
+				}
+			}, assert);
+		});
+
+		test('should parse 5-level ternaries mixed consequent/alternate', function (assert) {
+			testParser('a ? b ? 1 : c ? d ? e ? 3 : 4 : 5 : 6 : 7', {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'Identifier',
+					name: 'a'
+				},
+				consequent: {
+					type: 'ConditionalExpression',
+					test: {
+						type: 'Identifier',
+						name: 'b'
+					},
+					consequent: {
+						type: 'Literal',
+						value: 1,
+						raw: '1'
+					},
+					alternate: {
+						type: 'ConditionalExpression',
+						test: {
+							type: 'Identifier',
+							name: 'c'
+						},
+						consequent: {
+							type: 'ConditionalExpression',
+							test: {
+								type: 'Identifier',
+								name: 'd'
+							},
+							consequent: {
+								type: 'ConditionalExpression',
+								test: {
+									type: 'Identifier',
+									name: 'e'
+								},
+								consequent: {
+									type: 'Literal',
+									value: 3,
+									raw: '3'
+								},
+								alternate: {
+									type: 'Literal',
+									value: 4,
+									raw: '4'
+								}
+							},
+							alternate: {
+								type: 'Literal',
+								value: 5,
+								raw: '5'
+							}
+						},
+						alternate: {
+							type: 'Literal',
+							value: 6,
+							raw: '6'
+						}
+					}
+				},
+				alternate: {
+					type: 'Literal',
+					value: 7,
+					raw: '7'
+				}
+			}, assert);
 		});
 
 		[
